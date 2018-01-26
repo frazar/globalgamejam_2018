3a4cab2fefbb36b356a07dd96436475faffe1831
0100101a <32433865+0100101a@users.noreply.github.com>
2018-01-26 21:39:18 +0100

    prima versione gestore movimetni

---
+++ ProgettoMain/Assets/PlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    // Use this for initialization
+    
+    [Range(1,2)]
+    public int player;
+    public float moltiplicatore;// moltiplicagtore del movimento
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movimento = Vector3.zero;
+        if (Input.GetButton("su"+player))
+        {
+            movimento += Vector3.up * Time.deltaTime;
+        }
+
+        if (Input.GetButton("sotto" + player))
+        {
+            movimento += Vector3.down * Time.deltaTime;
+        }
+        if (Input.GetButton("destra" + player))
+        {
+            movimento += Vector3.right * Time.deltaTime;
+        }
+        if (Input.GetButton("sinistra" + player))
+        {
+            movimento += Vector3.left * Time.deltaTime;
+        }
+
+
+        transform.position += movimento * moltiplicatore;
+	}
+}
