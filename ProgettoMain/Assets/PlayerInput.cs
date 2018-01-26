20a79e8492b4464ff8dce380f05e951ba66adccd
Francesco Zardi <frazar0@hotmail.it>
2018-01-26 22:22:04 +0100

    Add sprites untore.png and sprite reversal

--- ProgettoMain/Assets/PlayerInput.cs
+++ ProgettoMain/Assets/PlayerInput.cs
@@ -17,22 +17,26 @@ public class PlayerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+
         Vector3 movimento = Vector3.zero;
         if (Input.GetButton("su"+player))
         {
             movimento += Vector3.up * Time.deltaTime;
         }
 
-        if (Input.GetButton("sotto" + player))
+        if (Input.GetButton("sotto" + player)) 
         {
             movimento += Vector3.down * Time.deltaTime;
         }
         if (Input.GetButton("destra" + player))
         {
+            gameObject.transform.localEulerAngles = new Vector3 (0, 180, 0);
             movimento += Vector3.right * Time.deltaTime;
         }
         if (Input.GetButton("sinistra" + player))
         {
+            gameObject.transform.localEulerAngles = new Vector3 (0, 0, 0);
             movimento += Vector3.left * Time.deltaTime;
         }
 
