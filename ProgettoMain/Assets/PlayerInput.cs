8628172d8d83530e5927885cbcce97557e66b065
Iron512 <ivanmartini96@gmail.com>
2018-01-27 09:45:02 +0100

    set up

--- ProgettoMain/Assets/PlayerInput.cs
+++ ProgettoMain/Assets/PlayerInput.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerInput : MonoBehaviour {
 
-    // Use this for initialization
-    
+    // Use this for initialization    
     [Range(1,2)]
     public int player;
     public float MOLTIPLICATORE;// moltiplicagtore del movimento
@@ -20,27 +20,22 @@ public class PlayerInput : MonoBehaviour {
 
 
         Vector3 movimento = Vector3.zero;
-        if (Input.GetButton("su" + player))
-        {
+        if (Input.GetButton("su" + player)) {
             movimento += Vector3.up * Time.deltaTime;
         }
 
-        if (Input.GetButton("sotto" + player)) 
-        {
+        if (Input.GetButton("sotto" + player)) {
             movimento += Vector3.down * Time.deltaTime;
         }
-        if (Input.GetButton("destra" + player))
-        {
+        if (Input.GetButton("destra" + player)) {
             gameObject.transform.localEulerAngles = new Vector3 (0, 180, 0);
             movimento += Vector3.right * Time.deltaTime;
         }
-        if (Input.GetButton("sinistra" + player))
-        {
+        if (Input.GetButton("sinistra" + player)) {
             gameObject.transform.localEulerAngles = new Vector3 (0, 0, 0);
             movimento += Vector3.left * Time.deltaTime;
         }
 
-
-        phy.velocity= movimento * MOLTIPLICATORE;
+        phy.velocity = movimento * MOLTIPLICATORE;
 	}
 }
