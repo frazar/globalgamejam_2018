fcb2c787457a940651e65d39439a5cbbf011715e
Iron512 <ivanmartini96@gmail.com>
2018-01-27 10:07:11 +0100

    update edificio

---
+++ ProgettoMain/Assets/Edificio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edificio : MonoBehaviour {
+	//determina se un edificio è infetto
+	private bool infetto;
+
+	// Use this for initialization
+	void Start () {
+		infetto = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void setInfetto () {
+		infetto = true;
+	}
+}
