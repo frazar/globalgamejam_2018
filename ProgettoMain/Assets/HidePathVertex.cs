6ff2012ba90583c55993890e0c5bacbd388d9711
Francesco Zardi <frazar0@hotmail.it>
2018-01-27 15:27:16 +0100

    Make it work

---
+++ ProgettoMain/Assets/HidePathVertex.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class HidePathVertex : MonoBehaviour {
+
+	void Start () {
+        if (Application.isPlaying) {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
