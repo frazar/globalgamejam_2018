e9534a5c6f3f377de6402067f2e143b563756bae
Gianluca <gianlucadestefano@outlook.it>
2018-01-28 11:21:35 +0100

    fov contadino

--- ProgettoMain/Assets/script/controlloreCamera.cs
+++ ProgettoMain/Assets/script/controlloreCamera.cs
@@ -6,7 +6,7 @@ public class controlloreCamera : MonoBehaviour {
 
     private Vector3 pos1, pos2;
 
-    public GameObject palyer1, player2;
+    public GameObject player1, player2;
     // Use this for initialization
 	void Start () {
       
@@ -14,7 +14,7 @@ public class controlloreCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        Vector3 pos1 = palyer1.transform.position;
+        Vector3 pos1 = player1.transform.position;
         Vector3 pos2 = player2.transform.position;
         Vector3 media = new Vector3((pos1.x + pos2.x) / 2, (pos1.y + pos2.y) / 2, -10);
         transform.position = media;
