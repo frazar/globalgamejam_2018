e04ddc84f3d16565cab152e988ac734ace51e9a3
Gianluca <gianlucadestefano@outlook.it>
2018-01-28 14:30:21 +0100

    commit_mappa

---
+++ ProgettoMain/Assets/script/gestorePartita.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gestorePartita {
+
+   
+    static int numeroNPC;
+    static int npcInfetti;
+    static int npcMorti;
+
+    public static void AggingiNPC()
+    {
+        numeroNPC++;
+    }
+
+    public static void AggiungiNPCInfetto() {
+        npcInfetti++;
+    }
+    public static void AggiuntinpcMorto()
+    {
+        npcMorti++;
+        if (npcMorti ==numeroNPC)
+        {
+            Debug.Log("VITTORIA");  
+        }
+    }
+}
