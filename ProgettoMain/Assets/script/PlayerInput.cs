e7c2edc61fc0f9cafa278c6f218c71004eba538c
Francesco Zardi <frazar0@hotmail.it>
2018-01-28 10:38:19 +0100

    Aggiungi suggerimento all'infezione

--- ProgettoMain/Assets/script/PlayerInput.cs
+++ ProgettoMain/Assets/script/PlayerInput.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Assertions;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -13,12 +14,19 @@ public class PlayerInput : MonoBehaviour {
     public float Moltiplicatore; // moltiplicagtore del movimento
     Rigidbody2D phy;
     float TempoIterazioneIniziale;
+    private GameObject testoSuggerimentoInfezione;
+
 	void Start () {
         phy = GetComponent<Rigidbody2D>();
+
+        testoSuggerimentoInfezione = GameObject.Find("/Main Camera/Canvas/TestoSuggerimentoInfezione");
+        Assert.IsNotNull(testoSuggerimentoInfezione);
 
         // Check that the playerIndex property was set correctly
         Assert.IsTrue(this.name == "player" + playerIndex);
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -58,24 +66,45 @@ public class PlayerInput : MonoBehaviour {
         }
     }
 
+    void OnTriggerEnter2D(Collider2D ColliderIn) {
+        switch (ColliderIn.gameObject.tag)
+        {
+            case GestoreTag.Edifici:  
+                // L'edificio può essere infettato, mostra all'utente il suggerimento
+                testoSuggerimentoInfezione.GetComponent<Text>().enabled = true;
+            break;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D ColliderIn) {
+        switch (ColliderIn.gameObject.tag)
+        {
+            case GestoreTag.Edifici:  
+                // L'edificio non può più essere infettato, nascondi all'utente il suggerimento
+                testoSuggerimentoInfezione.GetComponent<Text>().enabled = false;
+            break;
+        }        
+    }
+
+
     void ProcessoInfezioneEdifici(GameObject Edificio)
     {
         
         // Prima pressione del tasto azione, inizializza timer
         if (Input.GetButtonDown("azione" + playerIndex))
         {
-            // appena premuto il tasto azione
-          
+            // appena premuto il tasto azione          
             TempoIterazioneIniziale = Time.time;
         } 
         else if (Input.GetButton("azione" + playerIndex))
         {          
             // Il tasto viene tenuto giù 
+            Debug.Log("Infettato");
 
             Edificio edificio = Edificio.GetComponentInParent<Edificio>();
             if (Time.time - TempoIterazioneIniziale >= edificio.SecondiPerInfezione && !edificio.infetto)
-            {
-                edificio.setInfetto(true);
+            {   
+                edificio.infetta();
             }
         }
     }
